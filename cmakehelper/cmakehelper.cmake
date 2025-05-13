2605f9a9d8459723867369a24ea8bfd8859d0750
Philippe Sanche <philippe.sanche@gmail.com>
2025-05-13 15:12:05 -0400

    Add cmake helper
    -Moved out of another private repo

---
+++ cmakehelper/cmakehelper.cmake
@@ -0,0 +1,41 @@
+function(SetupCMake)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE INTERNAL "")
+    set(CMAKE_CXX_STANDARD 23 CACHE INTERNAL "")
+
+    find_package(Git REQUIRED)
+
+    message(STATUS CMAKE_CXX_COMPILER_ID="${CMAKE_CXX_COMPILER_ID}")
+    message(STATUS CMAKE_CXX_COMPILER="${CMAKE_CXX_COMPILER}")
+    message(STATUS CMAKE_CXX_STANDARD="${CMAKE_CXX_STANDARD}")
+endfunction()
+
+function(SetupCompilerFlags)
+    set (BASE_C_CXX_ADDITIONAL_FLAGS "-Wnull-dereference -Wdouble-promotion -Wshadow")
+    set (BASE_C_CXX_DISABLED_FLAGS "-Wno-unused-parameter")
+    # Don't use Wall here for all cases, for clang/cl it would convert to Weverything.
+    set (BASE_C_CXX_FLAGS "-Wpedantic -Wextra ${BASE_C_CXX_ADDITIONAL_FLAGS} ${BASE_C_CXX_DISABLED_FLAGS}")
+
+    # Clang-cl
+    if(CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+        set (CMAKE_CXX_FLAGS "${BASE_C_CXX_FLAGS} /Zi /W4" CACHE INTERNAL "")
+        set (CMAKE_C_FLAGS "${BASE_C_CXX_FLAGS} /Zi /W4" CACHE INTERNAL "")
+        # Msvc
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        set (CMAKE_CXX_FLAGS "${BASE_C_CXX_FLAGS} /Zi /W4" CACHE INTERNAL "")
+        set (CMAKE_C_FLAGS "${BASE_C_CXX_FLAGS} /Zi /W4" CACHE INTERNAL "")
+        # Clang, gcc
+    else ()
+        set (CMAKE_CXX_FLAGS "${BASE_C_CXX_FLAGS} -Wall" CACHE INTERNAL "" )
+        set (CMAKE_C_FLAGS "${BASE_C_CXX_FLAGS} -Wall" CACHE INTERNAL "" )
+    endif()
+endfunction()
+
+function(SetupAdditionalLibs)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        message(STATUS "Adding stdc++exp to linker options (-l)")
+        target_link_libraries(${PROJECT_NAME} stdc++exp)
+    endif()
+endfunction()
+
+
+
