585272b75449825401a1327e30c8c7973741698c
Philippe Sanche <philippe.sanche@gmail.com>
2025-05-15 12:09:40 -0400

    Set CMP0054 NEW in helper.

--- cmakehelper/cmakehelper.cmake
+++ cmakehelper/cmakehelper.cmake
@@ -1,3 +1,6 @@
+# "MSVC" comparison will fail if quoted variables get expanded.
+cmake_policy(SET CMP0054 NEW)
+
 function(SetupCMake)
     set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE INTERNAL "")
     set(CMAKE_CXX_STANDARD 23 CACHE INTERNAL "")
